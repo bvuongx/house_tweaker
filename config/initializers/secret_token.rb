88f31bc8350d9faa45da264cc65c6a698b43c250
bvuongx <bvuong@gmail.com>
2013-08-05 22:44:03 -0700

    first commit. added gem dependencies and rspec generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseTweak::Application.config.secret_token = '9acffa028cf2bf19c8ab8a9d14351022e2327eb04ddf90263a252348edae6f7fd4429c3a04f2d97a0f57bf27b87cab6e83beaeb7cd39f03b5af9019eb6ee0631'
